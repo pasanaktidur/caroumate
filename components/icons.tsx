a9f7a6b34767a98a5319f7077053741850f48f52
pasanaktidur <pasanaktidur@gmail.com>
2025-08-23 16:32:50 +0700

    feat(icons): Update text formatting icons
    Replaced existing SVG icons for bold, italic, and underline with new, more compact versions that better match the application's visual style.

--- components/icons.tsx
+++ components/icons.tsx
@@ -99,47 +99,62 @@ export const HomeIcon: React.FC<{ className?: string }> = ({ className }) => (
 
 // --- Text Formatting Icons ---
 export const BoldIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 4.5h5.25a3.75 3.75 0 0 1 0 7.5H5.25V4.5zm0 7.5h6.75a3.75 3.75 0 0 1 0 7.5H5.25v-7.5z" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path fillRule="evenodd" clipRule="evenodd" d="M2 1H8.625C11.0412 1 13 2.95875 13 5.375C13 6.08661 12.8301 6.75853 12.5287 7.35243C13.4313 8.15386 14 9.32301 14 10.625C14 13.0412 12.0412 15 9.625 15H2V1ZM5.5 9.75V11.5H9.625C10.1082 11.5 10.5 11.1082 10.5 10.625C10.5 10.1418 10.1082 9.75 9.625 9.75H5.5ZM5.5 6.25H8.625C9.10825 6.25 9.5 5.85825 9.5 5.375C9.5 4.89175 9.10825 4.5 8.625 4.5H5.5V6.25Z" />
     </svg>
 );
 export const ItalicIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 15m-7.5-15h6" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M14 1H5V4H7.75219L5.08553 12H2V15H11V12H8.24781L10.9145 4H14V1Z" />
     </svg>
 );
 export const UnderlineIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 4.5v7.5a3.75 3.75 0 0 0 7.5 0V4.5m-3.75 15h7.5" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M3 1V7C3 9.76142 5.23858 12 8 12C10.7614 12 13 9.76142 13 7V1H10V7C10 8.10457 9.10457 9 8 9C6.89543 9 6 8.10457 6 7V1H3Z" />
+        <path d="M14 16V14H2V16H14Z" />
     </svg>
 );
 export const StrikethroughIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M5 4.75C5 4.45531 5.16258 4.05336 5.69626 3.66792C6.22795 3.28392 7.03762 3 8 3C9.75028 3 10.7599 3.87319 10.9539 4.4663L13.8053 3.5337C13.0616 1.26011 10.5055 0 8 0C6.4771 0 5.03677 0.443615 3.93978 1.23588C2.84478 2.02672 2 3.24977 2 4.75C2 5.59786 2.26982 6.35719 2.70214 7H0V9H16V7H10.7035C9.87766 6.67447 8.95507 6.5 8 6.5C7.03762 6.5 6.22795 6.21608 5.69626 5.83208C5.16258 5.44664 5 5.04469 5 4.75Z" />
+        <path d="M11 11.25C11 11.1732 10.989 11.0892 10.9632 11H13.9921C13.9973 11.0824 14 11.1658 14 11.25C14 12.7502 13.1552 13.9733 12.0602 14.7641C10.9632 15.5564 9.5229 16 8 16C5.49455 16 2.93836 14.7399 2.19473 12.4663L5.0461 11.5337C5.24008 12.1268 6.24972 13 8 13C8.96238 13 9.77205 12.7161 10.3037 12.3321C10.8374 11.9466 11 11.5447 11 11.25Z" />
     </svg>
 );
 export const CaseIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h1.875M4.875 4.5h1.875m-1.875 0L6 6.375m1.5-1.875h1.875m1.875 0h1.875m-1.875 0L12 6.375m1.5-1.875h1.875m1.875 0h1.875m-1.875 0L18 6.375m-15 11.25h1.875m1.875 0h1.875m-1.875 0L6 15m1.5 1.875h1.875m1.875 0h1.875m-1.875 0L12 15m1.5 1.875h1.875m1.875 0h1.875m-1.875 0L18 15M21 21V3" />
+    <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M16.5142 2.7501C16.9323 2.75603 17.3026 3.0215 17.4425 3.41559L22.9425 18.9156C23.1271 19.4361 22.8549 20.0077 22.3344 20.1924C21.8139 20.3771 21.2423 20.1049 21.0576 19.5844L19.7857 16L12.8304 16L11.4326 19.611C11.2332 20.126 10.6541 20.3819 10.139 20.1826C9.62399 19.9832 9.36808 19.404 9.56746 18.889L15.5675 3.38901C15.7184 2.99904 16.0961 2.74417 16.5142 2.7501ZM16.4593 6.62545L13.6046 14L19.076 14L16.4593 6.62545Z" />
+        <path d="M8.25 20.2604C7.79939 20.2604 7.41867 19.9625 7.29352 19.5529C6.40302 20.0157 5.55589 20.2604 4.75 20.2604C2.77954 20.2604 1.25 18.8599 1.25 16.7604C1.25 15.8382 1.56906 15.0234 2.16035 14.4079C2.75035 13.7938 3.59693 13.3929 4.62959 13.2677C5.48568 13.1638 6.35256 13.21 7.22644 13.4026C7.18035 13.0648 7.06388 12.8244 6.88899 12.6546C6.65902 12.4314 6.28535 12.2882 5.70034 12.2591C4.75405 12.2121 4.148 12.3504 3.82178 12.5791C3.3696 12.8962 2.74598 12.7867 2.42888 12.3345C2.11179 11.8823 2.22131 11.2587 2.6735 10.9416C3.39111 10.4384 4.34684 10.2302 5.50465 10.2515L5.50864 10.2516L5.79966 10.2616C6.82486 10.3126 7.66878 10.6349 8.26882 11.2072C8.87029 11.7809 9.20249 12.5827 9.24525 13.5444L9.24544 13.55L9.25 13.7604V19.2604C9.25 19.7668 8.87372 20.185 8.38538 20.2513L8.37699 20.2524L8.2584 20.2604H8.25ZM4.87041 15.2895C4.29054 15.3598 3.88858 15.5454 3.63341 15.7967C3.38209 16.0442 3.25 16.3779 3.25 16.7967C3.25 17.2894 3.40867 17.6583 3.66052 17.9041C3.91333 18.1508 4.28388 18.2967 4.75 18.2967C5.35855 18.2967 6.14677 18.0129 7.11197 17.3755L7.249 17.282V15.5105L7.12728 15.4742C6.3497 15.2606 5.59939 15.201 4.87041 15.2895Z" />
     </svg>
 );
 export const AlignLeftIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M11 1H1V3H11V1Z" />
+        <path d="M1 5H15V7H1V5Z" />
+        <path d="M11 9H1V11H11V9Z" />
+        <path d="M15 13H1V15H15V13Z" />
     </svg>
 );
 export const AlignCenterIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-12.75 5.25h9" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M13 1H3V3H13V1Z" />
+        <path d="M1 5H15V7H1V5Z" />
+        <path d="M13 9H3V11H13V9Z" />
+        <path d="M15 13H1V15H15V13Z" />
     </svg>
 );
 export const AlignRightIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M15 1H5V3H15V1Z" />
+        <path d="M1 5H15V7H1V5Z" />
+        <path d="M15 9H5V11H15V9Z" />
+        <path d="M15 13H1V15H15V13Z" />
     </svg>
 );
 export const AlignJustifyIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M15 1H1V3H15V1Z" />
+        <path d="M1 5H15V7H1V5Z" />
+        <path d="M15 9H1V11H15V9Z" />
+        <path d="M11 13H1V15H11V13Z" />
     </svg>
 );
